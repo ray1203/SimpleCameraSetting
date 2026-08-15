84de86ef7f781c1e0c9daa2c9e51faf541fe2fdb
정재현 <ray1203@naver.com>
2026-08-15 18:17:10 +0900

    feat: 카메라 따라가기 기능에서 떨림 감소
    1. ConfigFixedUpdate_60 prefix에서 CameraDriver.Update postfix로 변경
    배속에서 프레임 어긋나 떨리던 문제 해결
    2. TrueCenter를 tweener.TweenedPos로 변경
    전투 중 카메라 떨림 등이 제거됨

--- CameraMapConfigUpdatePatch.cs
+++ CameraMapConfigUpdatePatch.cs
@@ -1,4 +1,4 @@
-﻿
+
 using UnityEngine;
 using Verse;
 using HarmonyLib;
@@ -9,65 +9,61 @@ namespace SimpleCameraSetting
 {
     //CameraFollowing 기능 관련
     //메시지 띄우기, 다른 림 Follow 시 부드럽게 이동 등
+    //ConfigFixedUpdate_60(60Hz)에선 배속일 때 폰이 떨려서 매 프레임 Update Postfix로 처리
+    //일단 클래스명과 파일명은 유지
     [HarmonyPatch]
     public static class CameraMapConfigUpdatePatch
     {
-        static List<Pawn> lastSelectedPawns=new List<Pawn>();
-        [HarmonyPatch(typeof(CameraMapConfig), nameof(CameraMapConfig.ConfigFixedUpdate_60))]
-        [HarmonyPrefix]
-        public static bool Prefix(ref CameraMapConfig __instance, ref Vector3 rootPos, ref Vector3 velocity)
+        static List<Pawn> lastSelectedPawns = new List<Pawn>();
+
+        [HarmonyPatch(typeof(CameraDriver), nameof(CameraDriver.Update))]
+        [HarmonyPostfix]
+        public static void FollowPostfix(CameraDriver __instance)
         {
-            if (__instance.followSelected)
+            //로딩중엔 바닐라 Update도 일찍 return하니 여기도 막음
+            if (LongEventHandler.ShouldWaitForEvent || Find.CurrentMap == null)
+                return;
+            CameraMapConfig config = __instance.config;
+            if (config == null || !config.followSelected)
+                return;
+
+            List<Pawn> selectedPawns = Find.Selector.SelectedPawns;
+
+            //선택 폰 없을 때
+            if (selectedPawns.Empty())
             {
-                List<Pawn> selectedPawns = Find.Selector.SelectedPawns;
-
-                //added code
-                if (selectedPawns.Empty())
+                if (SimpleCameraModSetting.modSetting.autoOffFollow)
                 {
-                    if (SimpleCameraModSetting.modSetting.autoOffFollow)
-                    {
-                        Current.CameraDriver.config.followSelected = false;
-                        if (SimpleCameraModSetting.modSetting.followMessage)
-                            Messages.Message("Camera Following " + (Current.CameraDriver.config.followSelected ? "On" : "Off"), new MessageTypeDef(),false);
-                    } 
-                    else
-                    {
-                        selectedPawns = lastSelectedPawns;
-                    }
-                }else lastSelectedPawns = new List<Pawn>(selectedPawns);
-                //code end
-                if (selectedPawns.Count > 0)
-                {
-                    Vector3 zero = Vector3.zero;
-                    int num = 0;
-                    foreach (Pawn t in selectedPawns)
-                    {
-                        if (t.MapHeld == Find.CurrentMap)
-                        {
-                            zero += t.TrueCenter();
-                            ++num;
-                        }
-                    }
-                    if (num > 0)
-                    {
-                        Vector3 target = zero / (float)num;
-                        target.y = rootPos.y;
-                        //original code
-                        //rootPos = Vector3.MoveTowards(rootPos, target, 0.02f * Mathf.Max(Find.TickManager.TickRateMultiplier, 1f) * this.moveSpeedScale);
-                        //Messages.Message("" + target, new MessageTypeDef());
+                    config.followSelected = false;
+                    if (SimpleCameraModSetting.modSetting.followMessage)
+                        Messages.Message("Camera Following Off", new MessageTypeDef(), false);
+                    return;
+                }
+                selectedPawns = lastSelectedPawns;
+            }
+            else { lastSelectedPawns.Clear(); lastSelectedPawns.AddRange(selectedPawns); }
 
-                        //added code
-                        Find.CameraDriver.JumpToCurrentMapLoc(target);
-                        //code end
-                    }
+            Vector3 zero = Vector3.zero;
+            int num = 0;
+            foreach (Pawn t in selectedPawns)
+            {
+                if (t.MapHeld == Find.CurrentMap)
+                {
+                    //이번 프레임 tween 갱신하고 읽어야 폰 그려지는 위치랑 카메라가 맞음
+                    //TweenedPos만 씀. DrawPos엔 피격 jitter/조준 lean 섞여서 카메라 흔들림
+                    t.Drawer.tweener.PreDrawPosCalculation();
+                    zero += t.Drawer.tweener.TweenedPos;
+                    ++num;
                 }
             }
-            if ((double)__instance.autoPanSpeed <= 0.0 || Find.TickManager.CurTimeSpeed == TimeSpeed.Paused && !__instance.autoPanWhilePaused)
-                return false;
-            velocity.x = Mathf.Cos(__instance.autoPanAngle) * __instance.autoPanSpeed;
-            velocity.z = Mathf.Sin(__instance.autoPanAngle) * __instance.autoPanSpeed;
-            return false;
+            if (num > 0)
+            {
+                Vector3 target = zero / (float)num;
+                //JumpToCurrentMapLoc으로 rootPos(카메라 위치) 세팅
+                __instance.JumpToCurrentMapLoc(target);
+                //Update의 위치적용은 이미 지나가서 같은 프레임 반영되게 다시 호출
+                Refs.applyPositionToGameObject(__instance);
+            }
         }
-
     }
 }
--- Refs.cs
+++ Refs.cs
@@ -1,5 +1,6 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using RimWorld;
+using System;
 using System.Reflection;
 using UnityEngine;
 using Verse;
@@ -11,10 +12,15 @@ namespace SimpleCameraSetting
     {
         public static readonly FieldInfo f_desiredSize;
         public static readonly AccessTools.FieldRef<CameraDriver, float> desiredSize;
+        //ApplyPositionToGameObject가 private라 리플렉션으로 뺌
+        //매 프레임 따라가기에서 카메라 위치 바로 반영하려고 씀
+        public static readonly Action<CameraDriver> applyPositionToGameObject;
         static Refs()
         {
             Refs.desiredSize = AccessTools.FieldRefAccess<CameraDriver, float>(nameof(desiredSize));
             Refs.f_desiredSize = AccessTools.Field(typeof(CameraDriver), nameof(desiredSize));
+            Refs.applyPositionToGameObject = AccessTools.MethodDelegate<Action<CameraDriver>>(
+                AccessTools.Method(typeof(CameraDriver), "ApplyPositionToGameObject"));
         }
     }
 }
